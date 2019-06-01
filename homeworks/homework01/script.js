7f8336c369538c69ee46e926390e89f8bf7e6691
Oksana Polevyk <oksanapolevyk@MacBook-Pro-Oksana.local>
2019-06-01 16:18:45 +0300

    added homework01

---
+++ homeworks/homework01/script.js
@@ -0,0 +1,9 @@
+console.log('Почему бы не посчитать, сколько получится, если 169*17-133')
+var result = 169*17-133;
+console.log('Результат:', result);
+console.log('%cА можно вот так выделить текст','font-size:16px;background:#92c3e9; color:#a83364;');
+console.groupCollapsed('Тыц!');
+console.info('А дальше я узнала, как выделять объекты и массивы:');
+console.log({year:2019, mounth:'may', day:15});
+console.log([1, 2, 3, 4, 5]);
+console.groupEnd();
